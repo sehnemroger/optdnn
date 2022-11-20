eb733d30fd3d2df074f74c59db85068b0326134e
sehnemroger <roger@sehnem.com>
2022-11-20 10:21:50 -0300

    docker_made_easy

---
+++ docker/build/CMakeFiles/user.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/user.dir/user.cxx.o"
+  "user"
+  "user.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/user.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
