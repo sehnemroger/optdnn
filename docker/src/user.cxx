bd3706f455b28af5de6fbe780f098afad294b31a
KenedyMatiasso <kenedyportella@hotmail.com>
2022-11-23 22:27:25 -0300

    Lattest changes. Not working

--- docker/src/user.cxx
+++ docker/src/user.cxx
@@ -1,20 +1,9 @@
 //////////////////////////////////////////////////////////////////////////
 ////////////////        Inveted Pendulum.cxx         /////////////////////
-//////////////////////////////////////////////////////////////////////////
-////////////////           Brought to you by:        /////////////////////
-///////////////             Roger M. Sehnem           ////////////////////
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-///// Title:      Optimal control to zero origin of inverted pendulum  ///
-//////// Last modified: 23 Decemvver 2022                 ////////////////
-//////// Reference:     My mathematica file               ////////////////
-//////////////////////////////////////////////////////////////////////////
-////////     Copyright (c) Roger M. Sehnem, 2022          ////////////////
-//////////////////////////////////////////////////////////////////////////
-//////// This is part of the PSOPT software library, which////////////////
-//////// is distributed under the terms of the GNU Lesser ////////////////
-//////// General Public License (LGPL)                    ////////////////
-//////////////////////////////////////////////////////////////////////////
+////////////////           Roger M. Sehnem           /////////////////////
+////////////////          Kenedy M. Portella         /////////////////////
+////// Title: Optimal control to zero origin of inverted pendulum  ///////
+
 
 #include "psopt.h"
 
@@ -24,23 +13,43 @@
 
 // Holds the constants of the inverted pendulum.
 struct Constants_pendulum {
-    adouble l;
-    adouble I;
-    adouble mb;
-    adouble mc;
-    adouble at;
-    adouble ar;
+    adouble rod_length;
+    adouble rod_inertia;
+    adouble rod_mass;
+    adouble cart_mass;
+    adouble translation_friction_coefficient;
+    adouble rotation_friction_coefficient;
 };
 
-typedef struct Constants_pendulum_;
+typedef struct Constants_pendulum Constants_pendulum_;
+
+// Holds the initial conditions of the inverted pendulum.
+struct initial_conditions {
+    adouble x1;
+    adouble x2;
+    adouble x3;
+    adouble x4;
+};
+
+typedef struct initial_conditions initial_conditions_;
 
 // Holds the constants for the optimal control formulation.
 struct Optimal_settings {
-    adouble r;
-    adouble q;
+    double r;
+    double q;
 };
 
-typedef struct Optimal_settings_;
+typedef struct Optimal_settings Optimal_settings_;
+
+// Holds the constants for the optimal control formulation.
+struct User_Data {
+    Optimal_settings_ Optimal_settings;
+    initial_conditions_ initial_conditions;
+    Constants_pendulum_ Constants_pendulum;
+};
+
+typedef struct User_Data User_Data_;
+
 
 //////////////////////////////////////////////////////////////////////////
 ///////////////////  Define the end point (Mayer) cost function //////////
@@ -63,21 +72,20 @@ adouble integrand_cost(adouble* states, adouble* controls, adouble* parameters,
     // Declare structure that holds parameters for the optimal control
     // formulation. Pure C++ whitchcraft, basically  magic that I once did
     // and don't remember what it means.
-    Optimal_settings_ &Optimal_settings = *( (Optimal_settings_ *) workspace->problem->user_data);
+    User_Data_ &User_Data = *( (User_Data_ *) workspace->problem->user_data);
 
     // The constants
-    const r = Optimal_settings.r;
-    const q = Optimal_settings.q;
+    double r = User_Data.Optimal_settings.r;
+    double q = User_Data.Optimal_settings.q;
 
-    // Defining the matrix needed for the cost function
-    const Q = q*eye(4);
-    const R = r*eye(4);
+    // Defining the matrices needed for the cost function
+    MatrixXd Q = q*eye(4);
+    MatrixXd R = r*eye(1);
     // This is a MatrixXd object, see the eigen package for details.
 
     // This is the cost function of the optimal control problem
-    return  states*Q*states.transpose() + constrols*R*constrols.transpose() + q*time;
+    return  states.transpose()*Q*states + constrols.transpose()*R*constrols + q*time;
 }
-
 
 //////////////////////////////////////////////////////////////////////////
 ///////////////////  Define the DAE's ////////////////////////////////////
@@ -89,16 +97,20 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
          adouble* xad, int iphase, Workspace* workspace)
 {
    // Structure that holds parameters of the pendulum
-   Constants_pendulum_ &Constants_pendulum = *( (Constants_pendulum_ *) workspace->problem->user_data);
+   User_Data_ &User_Data = *( (User_Data_ *) workspace->problem->user_data);
 
    // Extract parameters from the structure
-   adouble l = Constants_pendulum.l,  I = Constants_pendulum.I;
-   adouble mb = Constants_pendulum.mb, mc = Constants_pendulum.mc;
-   adouble at = Constants_pendulum.at, ar = Constants_pendulum.ar;
+   adouble l = User_Data.Constants_pendulum.rod_length;
+   adouble I = User_Data.Constants_pendulum.rod_inertia;
+   adouble mb = User_Data.Constants_pendulum.rod_mass;
+   adouble mc = User_Data.Constants_pendulum.cart_mass;
+   adouble at = User_Data.Constants_pendulum.translation_friction_coefficient;
+   adouble ar = User_Data.Constants_pendulum.rotation_friction_coefficient;
 
    // Defining helper variables
    adouble g = 9.8; // The acceleration of gravity
-   adouble C1 = l*mb, adouble C2 = I+pow(l,2)*mb;
+   adouble C1 = l*mb;
+   adouble C2 = I+pow(l,2)*mb;
    adouble C3 = mb+mc; // Those are just helper constants
 
    // Unpacking states
@@ -109,19 +121,17 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
 
    // Unpacking control
    adouble u = controls[ 0 ];
-
    adouble x1_dot, x2_dot, x3_dot, x4_dot;
 
    x1_dot = x2;
-   x2_dot = (g*C1**2*cos(x3)*sin(x3)+C2*(u-at*x2)-ar*C1*cos(x3)*x4-C1*C2*sin(x3)*x4**2)/(C2*C3-C1**2*cos(x3)**2);
+   x2_dot = (g*pow(C1,2)*cos(x3)*sin(x3)+C2*(u-at*x2)-ar*C1*cos(x3)*x4-C1*C2*sin(x3)*pow(x4,2))/(C2*C3-pow(C1,2)*pow(cos(x3),2));
    x3_dot = x4;
-   x4_dot = (g*C1*C3*sin(x3)+C1*cos(x3)*(u-at*x2)-ar*C3*x4-C1**2*cos(x3)*sin(x3)*x4**2)/(C2*C3-C1**2*cos(x3)**2);
+   x4_dot = (g*C1*C3*sin(x3)+C1*cos(x3)*(u-at*x2)-ar*C3*x4-pow(C1,2)*cos(x3)*sin(x3)*pow(x4,2))/(C2*C3-pow(C1,2)*pow(cos(x3),2));
 
    derivatives[ 0 ] = x1_dot;
    derivatives[ 1 ] = x2_dot;
    derivatives[ 2 ] = x3_dot;
    derivatives[ 3 ] = x4_dot;
-
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -131,22 +141,24 @@ void dae(adouble* derivatives, adouble* path, adouble* states,
 void events(adouble* e, adouble* initial_states, adouble* final_states,
             adouble* parameters,adouble& t0, adouble& tf, adouble* xad,
             int iphase, Workspace* workspace)
-
 {
-   adouble x0 = initial_states[ 0 ];
-   adouble y0 = initial_states[ 1 ];
-   adouble v0 = initial_states[ 2 ];
-   adouble xf = final_states[ 0 ];
-   adouble yf = final_states[ 1 ];
+   // Structure that holds initial conditions of the pendulum
+   User_Data_ &User_Data = *( (User_Data_ *) workspace->problem->user_data);
+   adouble x1_init = User_Data.initial_conditions.x1;
+   adouble x2_init = User_Data.initial_conditions.x2;
+   adouble x3_init = User_Data.initial_conditions.x3;
+   adouble x4_init = User_Data.initial_conditions.x4;
 
-   e[ 0 ] = x0;
-   e[ 1 ] = y0;
-   e[ 2 ] = v0;
-   e[ 3 ] = yf;
+   e[ 0 ] = x1_init - initial_states[0];
+   e[ 1 ] = x2_init - initial_states[1];
+   e[ 2 ] = x3_init - initial_states[2];
+   e[ 3 ] = x4_init - initial_states[3];
+   e[ 4 ] = final_states[0];
+   e[ 5 ] = final_states[1];
+   e[ 6 ] = final_states[2];
+   e[ 7 ] = final_states[3];
 
 }
-
-
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////  Define the phase linkages function ///////////////////
@@ -156,8 +168,6 @@ void linkages( adouble* linkages, adouble* xad, Workspace* workspace)
 {
   // No linkages as this is a single phase problem
 }
-
-
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Define the main routine ///////////////////////////////
@@ -178,8 +188,8 @@ int main(void)
 ///////////////////  Register problem name  ////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    problem.name        		= "Bryson Maximum Range Problem";
-    problem.outfilename                 = "brymr.txt";
+    problem.name        		= "Inverted pendulum problem";
+    problem.outfilename                 = "inv_pend_prob.txt";
 
 ////////////////////////////////////////////////////////////////////////////
 ////////////  Define problem level constants & do level 1 setup ////////////
@@ -194,10 +204,10 @@ int main(void)
 /////////   Define phase related information & do level 2 setup  ////////////
 /////////////////////////////////////////////////////////////////////////////
 
-    problem.phases(1).nstates   		= 3;
-    problem.phases(1).ncontrols 		= 2;
-    problem.phases(1).nevents   		= 4;
-    problem.phases(1).npath     		= 1;
+    problem.phases(1).nstates   		= 4;
+    problem.phases(1).ncontrols 		= 1;
+    problem.phases(1).nevents   		= 8;
+    problem.phases(1).npath     		= 0;
     problem.phases(1).nodes         << 50;
 
     psopt_level2_setup(problem, algorithm);
@@ -210,65 +220,85 @@ int main(void)
     MatrixXd lambda, H;
 
 ////////////////////////////////////////////////////////////////////////////
+//////////  Declare an instance structs/////////////
+////////////////////////////////////////////////////////////////////////////
+
+    initial_conditions_ initial_conditions;
+    Constants_pendulum_ Constants_pendulum;
+    Optimal_settings_ Optimal_settings;
+    User_Data_ User_Data;
+    
+    problem.user_data = (void*) &User_Data;
+
+////////////////////////////////////////////////////////////////////////////
+///////////////////  Initialize CONSTANTS and //////////////////////////////
+///////////////////  declare local variables  //////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+    Constants_pendulum.rod_length = 0.3;
+    Constants_pendulum.rod_inertia = 2;
+    Constants_pendulum.rod_mass = 1;
+    Constants_pendulum.cart_mass = 3;
+    Constants_pendulum.translation_friction_coefficient = 0.2;
+    Constants_pendulum.rotation_friction_coefficient = 0.2;
+
+    initial_conditions.x1 = 0.1;
+    initial_conditions.x2 = 0.1;
+    initial_conditions.x3 = 0.1;
+    initial_conditions.x4 = 0.02;
+
+    Optimal_settings.r = 1;
+    Optimal_settings.q = 1;
+
+    User_Data.Optimal_settings = Optimal_settings;
+    User_Data.initial_conditions = initial_conditions;
+    User_Data.Constants_pendulum = Constants_pendulum;
+
+////////////////////////////////////////////////////////////////////////////
 ///////////////////  Enter problem bounds information //////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    double xL = -10.0;
-    double yL = -10.0;
-    double vL = -10.0;
-    double xU = 10.0;
-    double yU = 10.0;
-    double vU = 10.0;
+    double x1_lower = -10.0;
+    double x2_lower = -100.0;
+    double x3_lower = -6.28;
+    double x4_lower = -6.28;
 
-    double u1L = -10.0;
-    double u2L = -10.0;
-    double u1U = 10.0;
-    double u2U = 10.0;
+    double x1_upper = 10.0;
+    double x2_upper = 100.0;
+    double x3_upper = 6.28;
+    double x4_upper = 6.28;   
 
-    double x0 = 0.0;
-    double y0 = 0.0;
-    double v0 = 0.0;
-    double yf = 0.1;
+    double u_lower = -5000;
+    double u_upper = 5000;
 
+    problem.phases(1).bounds.lower.states(0) = x1_lower;
+    problem.phases(1).bounds.lower.states(1) = x2_lower;
+    problem.phases(1).bounds.lower.states(2) = x3_lower;
+    problem.phases(1).bounds.lower.states(4) = x4_lower;
 
-    problem.phases(1).bounds.lower.states(0) = xL;
-    problem.phases(1).bounds.lower.states(1) = yL;
-    problem.phases(1).bounds.lower.states(2) = vL;
+    problem.phases(1).bounds.upper.states(0) = x1_upper;
+    problem.phases(1).bounds.upper.states(1) = x2_upper;
+    problem.phases(1).bounds.upper.states(2) = x3_upper;
+    problem.phases(1).bounds.upper.states(4) = x4_upper;
 
+    problem.phases(1).bounds.lower.controls(0) = u_lower;
+    problem.phases(1).bounds.upper.controls(0) = u_upper;
 
-    problem.phases(1).bounds.upper.states(0) = xU;
-    problem.phases(1).bounds.upper.states(1) = yU;
-    problem.phases(1).bounds.upper.states(2) = vU;
+    problem.phases(1).bounds.lower.events(0) = initial_conditions.x1;
+    problem.phases(1).bounds.lower.events(1) = initial_conditions.x2;
+    problem.phases(1).bounds.lower.events(2) = initial_conditions.x3;
+    problem.phases(1).bounds.lower.events(3) = initial_conditions.x4;
 
-
-    problem.phases(1).bounds.lower.controls(0) = u1L;
-    problem.phases(1).bounds.lower.controls(1) = u2L;
-    problem.phases(1).bounds.upper.controls(0) = u1U;
-    problem.phases(1).bounds.upper.controls(1) = u2U;
-
-    problem.phases(1).bounds.lower.events(0) = x0;
-    problem.phases(1).bounds.lower.events(1) = y0;
-    problem.phases(1).bounds.lower.events(2) = v0;
-    problem.phases(1).bounds.lower.events(3) = yf;
-
-
-    problem.phases(1).bounds.upper.events(0) = x0;
-    problem.phases(1).bounds.upper.events(1) = y0;
-    problem.phases(1).bounds.upper.events(2) = v0;
-    problem.phases(1).bounds.upper.events(3) = yf;
-
-    problem.phases(1).bounds.upper.path(0) = 1.0;
-    problem.phases(1).bounds.lower.path(0) = 1.0;
-
-
+    problem.phases(1).bounds.upper.events(0) = initial_conditions.x1;
+    problem.phases(1).bounds.upper.events(1) = initial_conditions.x2;
+    problem.phases(1).bounds.upper.events(2) = initial_conditions.x3;
+    problem.phases(1).bounds.upper.events(3) = initial_conditions.x4;
 
     problem.phases(1).bounds.lower.StartTime    = 0.0;
     problem.phases(1).bounds.upper.StartTime    = 0.0;
 
-    problem.phases(1).bounds.lower.EndTime      = 2.0;
-    problem.phases(1).bounds.upper.EndTime      = 2.0;
-
-
+    problem.phases(1).bounds.lower.EndTime      = 30.0;
+    problem.phases(1).bounds.upper.EndTime      = 30.0;
 
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////  Register problem functions  ///////////////////////////
@@ -285,19 +315,21 @@ int main(void)
 ///////////////////  Define & register initial guess ///////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    int nnodes    			             = problem.phases(1).nodes(0);
+    int nnodes    			            = problem.phases(1).nodes(0);
     int ncontrols                       = problem.phases(1).ncontrols;
     int nstates                         = problem.phases(1).nstates;
 
     MatrixXd x_guess    =  zeros(nstates,nnodes);
 
-    x_guess.row(0)  = x0*ones(1,nnodes);
-    x_guess.row(1)  = y0*ones(1,nnodes);
-    x_guess.row(2)  = v0*ones(1,nnodes);
+    x_guess.row(0)  = initial_conditions.x1*ones(1,nnodes);
+    x_guess.row(1)  = initial_conditions.x2*ones(1,nnodes);
+    x_guess.row(2)  = initial_conditions.x3*ones(1,nnodes);
+    x_guess.row(3)  = initial_conditions.x4*ones(1,nnodes);
+
 
     problem.phases(1).guess.controls       = zeros(ncontrols,nnodes);
     problem.phases(1).guess.states         = x_guess;
-    problem.phases(1).guess.time           = linspace(0.0,2.0,nnodes);
+    problem.phases(1).guess.time           = linspace(0.0,15.0,nnodes);
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -349,15 +381,15 @@ int main(void)
 ///////////  Plot some results if desired (requires gnuplot) ///////////////
 ////////////////////////////////////////////////////////////////////////////
 
-    plot(t,x,problem.name+": states", "time (s)", "states","x y v");
+//    plot(t,x,problem.name+": states", "time (s)", "states","x y v");
 
-    plot(t,u,problem.name+": controls","time (s)", "controls", "u_1 u_2");
+//     plot(t,u,problem.name+": controls","time (s)", "controls", "u_1 u_2");
 
-    plot(t,x,problem.name+": states", "time (s)", "states","x y v",
-                             "pdf", "brymr_states.pdf");
+//     plot(t,x,problem.name+": states", "time (s)", "states","x y v",
+//                              "pdf", "brymr_states.pdf");
 
-    plot(t,u,problem.name+": controls","time (s)", "controls", "u_1 u_2",
-                             "pdf", "brymr_controls.pdf");
+//     plot(t,u,problem.name+": controls","time (s)", "controls", "u_1 u_2",
+//                              "pdf", "brymr_controls.pdf");
 }
 
 ////////////////////////////////////////////////////////////////////////////
